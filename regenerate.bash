463d71b5031c20583d9c0bb5ca3b41a8e893aed9
Peter Bourgon <peter@bourgon.org>
2015-03-08 15:05:36 +0100

    Update index.html

--- regenerate.bash
+++ regenerate.bash
@@ -6,3 +6,8 @@ cat header.html > index.html
 curl -Ss -XGET 'https://raw.githubusercontent.com/peterbourgon/gokit/master/README.md' | pandoc --from markdown_github --to html >> index.html
 cat footer.html >> index.html
 
+for expr in 's/<br \/>/ /g' 's/[^\^]  / /g'
+do
+	cat index.html | sed -e "$expr" > index.html.fix
+	mv index.html.fix index.html
+done
